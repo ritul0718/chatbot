7562d6641df3cd185993398bfd3abc4e6c302a42
ritul0718 <164669584+ritul0718@users.noreply.github.com>
2024-03-26 14:56:15 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from PyPDF2 import PdfReader
 from streamlit_extras.add_vertical_space import add_vertical_space
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
 # Sidebar contents
@@ -32,7 +33,15 @@ def main():
         for page in pdf_reader.pages:
             text += page.extract_text()
 
-            st.write(text)
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+            )
+        chunks = text_splitter.split_text(text=text)
+        st.write(chunks)
+
+        # st.write(text)
 
 
 
